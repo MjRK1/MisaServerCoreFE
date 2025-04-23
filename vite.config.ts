a5edd373db72945bd9bbb34186cfd5f4692b289c
Oleg Sidorenko <sidorenko6503@gmail.com>
2025-04-23 13:22:32 +0300

    trying fix unavailable misaserver.ru

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     host: process.env.PROD_HOST ?? "localhost",
     allowedHosts: ['misaserver.ru']
   },
-  base: '/',
+  base: '/core',
   build: {
     outDir: 'dist',
     assetsInlineLimit: 0,
